843742a3c1f8a9f8f647c4f7e4827ee9cf06ab15
Nicolas Modrzyk <hellonico@gmail.com>
2021-05-21 18:22:42 +0900

    missing erosion

--- src/main/kotlin/dip/ErodingDilating.kt
+++ src/main/kotlin/dip/ErodingDilating.kt
@@ -16,7 +16,7 @@ fun main(args: Array<String>) {
         Size((2 * erosion_size + 1).toDouble(), (2 * erosion_size + 1).toDouble())
     )
     erode(source, destination, element)
-    imwrite("erosion.jpg", destination)
+    imwrite("out/erosion.jpg", destination)
     val dilation_size = 5
     val element1 = getStructuringElement(
         MORPH_RECT,
