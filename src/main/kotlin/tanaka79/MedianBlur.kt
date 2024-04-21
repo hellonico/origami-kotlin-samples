8adcb647d64ace1b6909d107a80257f6ee599650
Nicolas Modrzyk <hellonico@gmail.com>
2024-04-21 21:20:37 +0900

    tanaka

--- src/main/kotlin/tanaka79/MedianBlur.kt
+++ src/main/kotlin/tanaka79/MedianBlur.kt
@@ -11,6 +11,6 @@ fun main(args: Array<String>) {
     Origami.init()
     val im = imread("data/lupin3.jpeg") // 入力画像の取得
     val dst = Mat()
-    medianBlur(im, dst, 5)
+    medianBlur(im, dst, 19)
     imwrite("out/tanaka_median.jpg", dst) // 出力画像の保存
 }
